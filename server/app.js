df12ff17f289bdde821b2b606513598c49f78675
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-05 07:58:50 +0600

    Basic GraphQL Schema prepared

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = require('./schema/schema');
 
 const app = express();
 
+app.use('/graphql', graphqlHTTP({
+	schema
+}));
 
 const port = 5000;
 
